2004b7a49d3413c64425c0d0d8a2acce14f039a1
tmadura <48869682+tmadura@users.noreply.github.com>
2019-03-24 12:32:20 +0100

    Update BubbleSorty.java

--- MyFirstJavaProject/src/ppj/git/BubbleSorty.java
+++ MyFirstJavaProject/src/ppj/git/BubbleSorty.java
@@ -7,7 +7,7 @@ public class BubbleSorty {
 	{
 		for (int j=0; j<arr.length - 1; j++) {
 			for (int i=0; i<arr.length - 1; i++) {
-				if (arr[i] > arr[i+1]) {
+				if (arr[i] < arr[i+1]) {
 					int temp = arr[i];
 					arr[i] = arr[i + 1];
 					arr[i + 1] = temp;
